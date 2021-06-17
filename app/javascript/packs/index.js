fba0ca6dd0634cdaaed2840ab6702b08ec28fdf9
seryl lounis <seryllounis@outlook.fr>
2021-06-17 18:40:30 +0200

    config

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -5,6 +5,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from './App';
+import '../../assets/stylesheets/pages.scss';
 
 document.addEventListener("DOMContentLoaded", () => {
   ReactDOM.render(
