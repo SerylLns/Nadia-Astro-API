cf934d814d854e3a33b5e1f1c29f8537fd03cc6c
seryl lounis <seryllounis@outlook.fr>
2021-06-17 12:12:42 +0200

    tailwind config

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,4 +12,5 @@ Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
-import "../layouts/application.css";
+// import "../layouts/application.css";
+import "../../assets/stylesheets/pages.scss";
